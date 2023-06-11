ebeff1e59920869475ff6a4545e919aa1ef9fbb6
generalkhun <khun2551@hotmail.com>
2023-06-11 15:17:18 +0700

    add save to google sheet api and some mocking data on form submittion

---
+++ kangnai-fest-regis/src/types/formData.d.ts
@@ -0,0 +1,11 @@
+export interface FormRegisData {
+    timestamp: string;
+    id: string;
+    nickname: string;
+    name: string;
+    surname: string;
+    dateOfBirth: string;
+    contact: string;
+    howDidYouFindUs: string;
+    whatIsYourInterested: string;
+}
