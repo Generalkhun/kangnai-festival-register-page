9b5e12d73bdb538aea1c3522dcf5cc50c83ef5e9
ThananyaSih <thananya.sih@gmail.com>
2023-06-12 15:06:47 +0700

    create intro page and styles

--- kangnai-fest-regis/src/app/introduce-vol.1/page.tsx
+++ kangnai-fest-regis/src/app/introduce-vol.1/page.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
 import React from "react";
+
 import { ProceedingButton } from "@/components/ProceedingButton";
+import { introBtnText } from "@/constants";
+
+import introHead from "/public/img/intro-head.png";
 
 type Props = {};
 
@@ -8,11 +12,13 @@ const index = (props: Props) => {
   return (
     <div>
       <div className="regis-container">
-        <div className="head-container">introduce page</div>
+        <div className="head-container">
+          {/* <image src={introHead} alt="Introduce Head" /> */}
 
-        <Link href={"/emotional-check"}>
-          <ProceedingButton buttonText={`ทักทายความพัง`} />
-        </Link>
+          <Link href={"/emotional-check"}>
+            <ProceedingButton buttonText={introBtnText} />
+          </Link>
+        </div>
       </div>
     </div>
   );
