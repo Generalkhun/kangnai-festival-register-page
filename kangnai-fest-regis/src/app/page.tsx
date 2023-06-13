6cbdc775d88c43e120eb3f29d097800c4e93e8ec
ThananyaSih <thananya.sih@gmail.com>
2023-06-13 13:01:25 +0700

    change to svg

--- kangnai-fest-regis/src/app/page.tsx
+++ kangnai-fest-regis/src/app/page.tsx
@@ -12,22 +12,27 @@ import { headText2 } from "../constants";
 import festLogo from "/public/img/LOGO.svg";
 import birds from "/public/img/birds.svg";
 import bgWhite from "/public/img/bg-white.svg";
-import spon from "/public/img/bg-white.svg"
-import breeze from "/public/img/spon.png";
+import human from "/public/img/human.svg";
+import breeze from "/public/img/breeze.svg";
+import cloud1 from "/public/img/cloud1.svg";
+import cloud2 from "/public/img/cloud2.svg";
+import line from "/public/img/เส้นคั่น.svg";
+import spon1 from "/public/img/s.svg";
+import spon2 from "/public/img/breeze.svg";
+import spon3 from "/public/img/breeze.svg";
 //----------------------------------------------//
 
 export default function Home() {
   return (
     <div>
       <div className="home-container">
-        {/* 
         <Image id="festLogo" src={festLogo} alt="Fest Logo" />
         <Image id="bg-white" src={bgWhite} alt="bg-white" />
         <Image id="human" src={human} alt="human" />
         <Image id="cloud1" src={cloud1} alt="cloud1" />
         <Image id="cloud2" src={cloud2} alt="cloud2" />
         <Image id="breeze" src={breeze} alt="breeze" />
-        <Image id="birds" src={birds} alt="Three birds" /> */}
+        <Image id="birds" src={birds} alt="Three birds" />
 
         <div className="body-container">
           <h3>{headText1}</h3>
@@ -41,21 +46,21 @@ export default function Home() {
             <WalkInButton buttonText={walkInBtnText} />
           </Link>
         </div>
-        {/* <ul className="foot-container">
+        <ul className="foot-container">
           <li className="list-spon1">
-            <Image src={spon1} alt="spon1" />
+            <Image src={} alt="spon1" />
           </li>
           <Image src={line} alt="line" />
 
           <li className="list-spon2">
-            <Image src={spon2} alt="spon2" />
+            <Image src={} alt="spon2" />
           </li>
           <Image src={line} alt="line" />
 
           <li className="list-spon3">
-            <Image src={spon3} alt="spon3" />
+            <Image src={} alt="spon3" />
           </li>
-        </ul> */}
+        </ul>
       </div>
     </div>
   );
