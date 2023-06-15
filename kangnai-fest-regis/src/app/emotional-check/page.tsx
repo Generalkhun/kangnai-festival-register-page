52bfc1fa5f98e99024723ed044fc70c08ef296a5
generalkhun <khun2551@hotmail.com>
2023-06-15 16:44:28 +0700

    add emotion words list

--- kangnai-fest-regis/src/app/emotional-check/page.tsx
+++ kangnai-fest-regis/src/app/emotional-check/page.tsx
@@ -1,18 +1,57 @@
+"use client"
 import { ProceedingButton } from "@/components/Button";
 import Link from "next/link";
-import React from "react";
+import React, { useCallback, useMemo, useState } from "react";
+import Image from "next/image";
+import "./page.css";
+import bg01 from "/public/img/BG01.png";
+import { emotionalPageHeaderText, emotionWords } from "@/constants";
+import { EmotionCircle } from "@/components/EmotionCircle";
 
 type Props = {};
 
 const index = (props: Props) => {
-  return (
-    <div>
-      emotional check
-      <Link href={"/feeling-card"}>
-        <ProceedingButton buttonText={`ต่อไป`} />
-      </Link>
-    </div>
-  );
+    const [selectedEmotionWord, setSelectedEmotionWord] = useState<string>('')
+    const onSelectEmotion = (word: string) => {
+        setSelectedEmotionWord(word)
+    }
+    const renderEmotionCircle = useCallback((word:string) => (
+        <EmotionCircle
+            onSelectEmotion={onSelectEmotion}
+            isSelected={selectedEmotionWord === word}
+            emotionWord={word}
+        />
+    ), [selectedEmotionWord, onSelectEmotion])
+    return (
+        <div className="page-container">
+            <div className="text-container">
+                <div className="text">
+                    {emotionalPageHeaderText}
+                </div>
+            </div>
+            <div className="emotion-circle-container">
+                <div className="emotion-circle-rows-odd">
+                    {emotionWords.row1.map((word: string) => renderEmotionCircle(word))}
+                </div>
+                <div className="emotion-circle-rows-even">
+                    {emotionWords.row2.map((word: string) => renderEmotionCircle(word))}
+                </div>
+                <div className="emotion-circle-rows-odd">
+                    {emotionWords.row3.map((word: string) => renderEmotionCircle(word))}
+                </div>
+                <div className="emotion-circle-rows-even">
+                    {emotionWords.row4.map((word: string) => renderEmotionCircle(word))}
+                </div>
+                <div className="emotion-circle-rows-odd">
+                    {emotionWords.row5.map((word: string) => renderEmotionCircle(word))}
+                </div>
+            </div>
+            <Image id="bg01" src={bg01} alt="bg-02" />
+            {selectedEmotionWord.length && <Link href={"/feeling-card"}>
+                <ProceedingButton buttonText={`ต่อไป`} />
+            </Link>}
+        </div>
+    );
 };
 
 export default index;
