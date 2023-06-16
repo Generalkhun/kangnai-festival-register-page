bd8831d3ee7cbfe2e8096f6ae857f8bc0e19cf68
generalkhun <khun2551@hotmail.com>
2023-06-16 16:11:24 +0700

    d

--- kangnai-fest-regis/src/app/feeling-card/page.tsx
+++ kangnai-fest-regis/src/app/feeling-card/page.tsx
@@ -23,6 +23,7 @@ import quality15 from "public/img/quality15.svg"
 import quality16 from "public/img/quality16.svg"
 import quality17 from "public/img/quality17.svg"
 import quality18 from "public/img/quality18.svg"
+import whiteunderliner from "public/img/whiteunderliner.svg";
 import Image from "next/image";
 import './page.css'
 import { cardContentCreator } from '@/constants';
@@ -133,9 +134,16 @@ const Page = () => {
                     {renderCard(cardNumber)}
                     <div style={{
                         position: "absolute",
-                        top: "629px",
-                        left: "50px"
+                        top: "620px",
+                        left: "5px",
+                        padding: "0px 90px",
+                        textAlign: "center",
+                        fontWeight: "600px",
+                        fontSize: "20px",
+                        lineHeight: "30px",
                     }}>
+                        <Image id="underline" src={whiteunderliner} alt="underline" />
+                        <br />
                         {cardContent}
                     </div>
                     <Link href={'/'}>
